ed5fc26f413b93a74d9e7436e187b53dfff3d0ee
Adriel Teles <adrielteles@outlook.com>
2023-09-05 21:32:23 -0300

    Check Logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,15 +19,12 @@ public class Program {
             try{
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
-                System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(scan);
 
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
-                System.out.println();
                 System.out.print("Target: ");
 
                 ChessPosition target = UI.readChessPosition(scan);
